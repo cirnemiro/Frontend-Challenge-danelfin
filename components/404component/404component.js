b353118d259bed70dbccbe3ea86bc8183b50ab11
enric <cirnemiro@gmail.com>
2025-01-21 21:46:02 +0100

    fix(i18n): translations now works for page 404 and ModalLastUpdate

--- components/404component/404component.js
+++ components/404component/404component.js
@@ -1,43 +1,67 @@
-"use client"
+"use client";
 
 import dynamic from "next/dynamic";
-import { useTranslation } from 'next-i18next';
-import Link from 'next/link';
-const ErrorBoundary = dynamic(() => import('@/components/ErrorBoundary'))
-import styles from './404componentStyles';
+import Link from "next/link";
+const ErrorBoundary = dynamic(() => import("@/components/ErrorBoundary"));
+import styles from "./404componentStyles";
+import { useTranslation } from "@/src/app/i18n/client";
 
-const PageNotFound = ({ statusCode, error, api}) => {
-    const { t } = useTranslation('common');
+const PageNotFound = ({ statusCode, error, api }) => {
+  const { t } = useTranslation("common");
 
-    const linkMaker = (href, title, text) => <span className="bottom_links">
-        <Link href={href} legacyBehavior>
-            <a title={title}>{text}</a>
-        </Link>
-        <style jsx>{styles}</style>
+  const linkMaker = (href, title, text) => (
+    <span className="bottom_links">
+      <Link href={href} legacyBehavior>
+        <a title={title}>{text}</a>
+      </Link>
+      <style jsx>{styles}</style>
     </span>
+  );
 
-    return (
-            <>
-                <div className="page-not-found-container">
-                    <div className="page-not-found-content">
-                        <img alt="404 robot" src={'/images/bgImg/robot404.jpg'} className="robot_404" />
-                        <div className="desc_404">
-                            <span className="opps_text">{t('pageNotFound.opps')}</span>
-                            <span className="text_nf">{t('pageNotFound.pageNF')}</span>
-                            <span className="search_others">{t('pageNotFound.searchOthers')}</span>
-                            <span className="others_options">
-                                {linkMaker('/us-stocks', t('navbar.anchorTitles.usStocks'), t('footer.tops.us'))}
-                                {linkMaker('/european-stocks', t('navbar.anchorTitles.euStocks'), t('footer.tops.european'))}
-                                {linkMaker('/trade-ideas', t('tradeIdeas.seo.title'), t('footer.bottomNavbar.trade'))}
-                                {linkMaker('/stocks/investment-themes/', t('investmentThemes.seo.title'), t('sectors.columnHeaders.investment'))}
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <ErrorBoundary statusCode={'404'} error={error} api={api}/>
-                <style jsx>{styles}</style>
-            </>
-    )
-}
+  return (
+    <>
+      <div className="page-not-found-container">
+        <div className="page-not-found-content">
+          <img
+            alt="404 robot"
+            src={"/images/bgImg/robot404.jpg"}
+            className="robot_404"
+          />
+          <div className="desc_404">
+            <span className="opps_text">{t("pageNotFound.opps")}</span>
+            <span className="text_nf">{t("pageNotFound.pageNF")}</span>
+            <span className="search_others">
+              {t("pageNotFound.searchOthers")}
+            </span>
+            <span className="others_options">
+              {linkMaker(
+                "/us-stocks",
+                t("navbar.anchorTitles.usStocks"),
+                t("footer.tops.us")
+              )}
+              {linkMaker(
+                "/european-stocks",
+                t("navbar.anchorTitles.euStocks"),
+                t("footer.tops.european")
+              )}
+              {linkMaker(
+                "/trade-ideas",
+                t("tradeIdeas.seo.title"),
+                t("footer.bottomNavbar.trade")
+              )}
+              {linkMaker(
+                "/stocks/investment-themes/",
+                t("investmentThemes.seo.title"),
+                t("sectors.columnHeaders.investment")
+              )}
+            </span>
+          </div>
+        </div>
+      </div>
+      <ErrorBoundary statusCode={"404"} error={error} api={api} />
+      <style jsx>{styles}</style>
+    </>
+  );
+};
 
 export default PageNotFound;
--- components/ModalLastUpdate/ModalLastUpdate.js
+++ components/ModalLastUpdate/ModalLastUpdate.js
@@ -1,25 +1,34 @@
-"use client"
-import {useTranslation} from "next-i18next";
-import { Modal, Button } from 'react-bootstrap';
+"use client";
 
-const ModalLastUpdate = ({showModal, setShowModal}) => {
-    const {t} = useTranslation('common');
+import { useTranslation } from "@/src/app/i18n/client";
+import { Modal, Button } from "react-bootstrap";
 
-    return (
-        <Modal show={showModal} onHide={() => setShowModal(false)} className="market-filter-modal" centered>
-            <Modal.Header>
-                <Modal.Title>{t('stockMarketFilter.modals.modalDateTitle')}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <p>{t('stockMarketFilter.modals.description')}</p>
-                {t('stockMarketFilter.modals.modalDateBody')}</Modal.Body>
-            <Modal.Footer>
-                <Button variant="primary" onClick={() => setShowModal(false)}>
-                    {t('stockMarketFilter.modals.close')}
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    )
-}
+const ModalLastUpdate = ({ showModal, setShowModal }) => {
+  const { t } = useTranslation("common");
 
-export default ModalLastUpdate
+  return (
+    <Modal
+      show={showModal}
+      onHide={() => setShowModal(false)}
+      className="market-filter-modal"
+      centered
+    >
+      <Modal.Header>
+        <Modal.Title>
+          {t("stockMarketFilter.modals.modalDateTitle")}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{t("stockMarketFilter.modals.description")}</p>
+        {t("stockMarketFilter.modals.modalDateBody")}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={() => setShowModal(false)}>
+          {t("stockMarketFilter.modals.close")}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalLastUpdate;
